7f217f830c2f2ef8d7241526a62cf2869c91ec14
wkrud <samlee8482@gmail.com>
2020-06-28 05:57:57 +0900

    Sam : Set DB&DTO-1

---
+++ Project_samstore/src/main/java/sam/project/samstore/dto/MemberDTO.java
@@ -0,0 +1,15 @@
+package sam.project.samstore.dto;
+
+public class MemberDTO {
+	private int mb_uid;
+	private String mb_id;
+	private String mb_pw;
+	private String mb_name;
+	private String mb_img_org;
+	private String mb_img_sav;
+	private String mb_tel;
+	private String mb_email;
+	private String mb_regDate;
+	private String mb_address;
+	private int mb_type;
+}
---
+++ Project_samstore/src/main/java/sam/project/samstore/dto/MerchandiseDTO.java
@@ -0,0 +1,5 @@
+package sam.project.samstore.dto;
+
+public class MerchandiseDTO {
+	
+}
